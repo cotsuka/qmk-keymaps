5a409dd8c4bd74dbd11786fd675379daca2b46e8
Cameron Otsuka <cameron@otsuka.haus>
2025-02-11 19:57:05 -0800

    add keychron q10

---
+++ keyboards/keychron/q10/ansi_encoder/keymaps/cotsuka/config.h
@@ -0,0 +1,10 @@
+# pragma once
+
+// Turn RGBs off by default
+#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_NONE
+
+// Tap-hold configuration for home row mods.
+#define TAPPING_TERM 225
+#define TAPPING_TERM_PER_KEY
+#define PERMISSIVE_HOLD
+#define QUICK_TAP_TERM_PER_KEY
--- keyboards/keychron/q10/ansi_encoder/keymaps/cotsuka/rules.mk
+++ keyboards/keychron/q10/ansi_encoder/keymaps/cotsuka/rules.mk
@@ -1,1 +1,6 @@
+# build setup
+LTO_ENABLE = yes
+
+# features
+AUTOCORRECT_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
